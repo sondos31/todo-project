c986bcf2b6dcc2134918b014072149e19e7c9554
sondos31 <sondosmajid31@gmail.com>
2022-04-20 02:09:28 +0300

    done

---
+++ app.js
@@ -0,0 +1,18 @@
+
+var name=prompt('Enter your name');
+var gender=prompt('Enter your gender(male or female)');
+var age=prompt('Enter your age');
+if (age <=0){
+ alert('wrong age , plz reEnter the age') ;
+}
+var result =confirm('Do you want to skip the welcoming message?');
+if(result=='no'|| result=='NO'){
+if
+(gender=='male ' || gender==' Male' )
+    alert('Welcome MR '+ name + ' Here');
+else if (gender=='female' || gender=='Female' )
+alert('Welcome MRS ' + name + ' Here');
+
+}
+else
+ alert('Welcome ' + name +  ' Here' );
